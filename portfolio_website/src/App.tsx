a1f4c16ea9d6724e54cf844ff79563b3ead34ad1
KawishGangadin <100194278+KawishGangadin@users.noreply.github.com>
2024-11-10 22:15:50 +0100

    Filled in about me section.

--- portfolio_website/src/App.tsx
+++ portfolio_website/src/App.tsx
@@ -7,7 +7,17 @@ function App() {
       <div className='app'>
         <div className='app-header'>
           <div className='about-me'>
-            <div className='header'>  
+            <div className='header'>
+              <circle className='profile-picture'></circle>  
+              <div className='info'>
+                <h1>Kawish Shiwan Gangadin</h1>
+                <p>I am a fullstack software developer.</p>
+                <p>Specializing in creating innovative</p>
+                <p>web-applications.</p>
+              </div>  
+            </div>
+            <div className='footer'>
+              <p>📍 Rotterdam, The Netherlands</p>
             </div>
           </div>
           <div className='skills'></div>
